059a582305aaf17fe391e429a61fc94c38b18b7a
WASTEDeveloper <a.shabalinski@gmail.com>
2020-12-17 12:36:00 +0300

    Добавьте файлы проекта.

---
+++ srezKPandYP/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srezKPandYP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = Reader.GetArray();
+
+            foreach (var item in mas)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            Reader.ReplaceOddElements(mas, 0);
+
+            Reader.SaveToFile(mas, "out.txt");
+        }
+    }
+}
---
+++ srezKPandYP/Reader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srezKPandYP
+{
+    class Reader
+    {
+        public static int MasSize { get; set; }
+        public static int[] GetArray()
+        {   
+            MasSize = int.Parse(File.ReadAllLines("in.txt").First());
+            int[] mas = new int[MasSize];
+
+            int iter = 0;
+            foreach (var item in File.ReadAllLines("in.txt").Skip(1))
+            {
+                mas[iter] = int.Parse(item);
+                iter++;
+            }
+
+            return mas;
+        }
+
+        public static void ReplaceOddElements(int[] mas, int value)
+        {
+            for (int i = 0; i < mas.Length; i++)
+            {
+                if (mas[i] % 2 != 0)
+                {
+                    mas[i] = value;
+                }
+            }
+        }
+
+        public static void SaveToFile(int[] mas, string path)
+        {
+            string[] mass = new string[MasSize];
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mass[i] = mas[i].ToString();
+            }
+            File.WriteAllLines(path, mass);
+        }
+    }
+}
